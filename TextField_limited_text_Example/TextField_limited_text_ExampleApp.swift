3249a24af6a49b166861bfa0f58482eab6c564fa
katafuchix <katafuchix@gmail.com>
2023-01-10 13:39:17 +0900

    Initial Commit

---
+++ TextField_limited_text_Example/TextField_limited_text_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextField_limited_text_ExampleApp.swift
+//  TextField_limited_text_Example
+//
+//  Created by cano on 2023/01/10.
+//
+
+import SwiftUI
+
+@main
+struct TextField_limited_text_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
